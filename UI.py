bda2396da8f8c791c828c323fdf1cb34788422d1
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-11-11 17:16:08 +0900

    UI

---
+++ UI.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+
+class MyApp(QWidget):
+
+  def __init__(self):
+      super().__init__()
+      self.initUI()
+
+  def initUI(self):
+      self.setWindowTitle('RASZAS')
+      self.setWindowIcon(QIcon('RASZASIcon.JPG'))
+      self.setGeometry(300, 300, 300, 200)
+      self.resize(1000,1000)
+      self.show()
+
+
+if __name__ == '__main__':
+  app = QApplication(sys.argv)
+  ex = MyApp()
+  sys.exit(app.exec_())
