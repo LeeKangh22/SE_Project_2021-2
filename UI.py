5b66824d6cdf94e5f97d91fa02c5d80007f364ea
LeeKangh22 <dlrkdgml1998@dongguk.edu>
2021-11-11 17:29:08 +0900

    UI btn

--- UI.py
+++ UI.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtGui import QIcon
-
+from PyQt5.QtCore import QCoreApplication
 
 class MyApp(QWidget):
 
@@ -10,10 +10,18 @@ class MyApp(QWidget):
       self.initUI()
 
   def initUI(self):
+      btnAdmin = QPushButton('관리자로 로그인', self)
+      btnAdmin.move(400,350)
+      btnAdmin.resize(200,50)
+      #btnAdmin.clicked.connect(QCoreApplication.instance().quit)
+      btnStaff = QPushButton('직원으로 로그인', self)
+      btnStaff.move(400,420)
+      btnStaff.resize(200,50)
       self.setWindowTitle('RASZAS')
       self.setWindowIcon(QIcon('RASZASIcon.JPG'))
       self.setGeometry(300, 300, 300, 200)
-      self.resize(1000,1000)
+      self.resize(1000,500)
+      
       self.show()
 
 
