92aeafd538ee3d98ce404b77d3b545adb775bd00
HoJun <sson514@naver.com>
2021-11-12 19:21:32 +0900

    2021-11-12 DB연동

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("test")
+# DB test
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+import django
+django.setup()
+
+from model.models import *
+
+hojun = Staff.objects.get(id = 1)
+
+print(hojun.phone_num)
+    
