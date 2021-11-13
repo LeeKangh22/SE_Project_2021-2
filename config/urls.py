0bdf5931a6193720e73ce42851689766f1fdaff1
gonasoo <happysun7080@naver.com>
2021-11-13 18:43:55 +0900

    2021-11-13 update version

--- config/urls.py
+++ config/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),    
 ]
--- model/views.py
+++ model/views.py
@@ -2,5 +2,4 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def getStaffList(request, sid):
-    return HttpResponse("for test %s" % sid)
+
