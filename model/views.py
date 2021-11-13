6b4a538157d98a421c9b262f7a9b9ee6ed89b90e
gonasoo <happysun7080@naver.com>
2021-11-13 16:39:13 +0900

    211113 superuser 생성

--- model/views.py
+++ model/views.py
@@ -1,3 +1,6 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def getStaffList(request, sid):
+    return HttpResponse("for test %s" % sid)
